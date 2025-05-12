0631ba356ad1282feb7568ac6d16ebbff9ba422c
Rémi Delgatte <remi.delgatte@gmail.com>
2025-05-12 08:42:55 +0200

    chore(type): remove prefix from model type definition (#2)

--- app/Model.hs
+++ app/Model.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Model (TodoItem (..), TodoItemSpec (..)) where
 
 import Data.Aeson ((.:), (.:?))
 import qualified Data.Aeson as Aeson
+import Data.Aeson.Types (genericToJSON)
+import Data.Char (toLower)
 import Data.Int (Int64)
 import qualified Data.Text as T
 import GHC.Generics (Generic)
@@ -17,7 +19,8 @@ data TodoItem = TodoItem
   }
   deriving (Generic, Show, Eq)
 
-instance Aeson.ToJSON TodoItem
+instance Aeson.ToJSON TodoItem where
+  toJSON = genericToJSON $ aesonOptions "TodoItem"
 
 data TodoItemSpec = TodoItemSpec
   { todoItemSpecTitle :: T.Text,
@@ -29,3 +32,16 @@ instance Aeson.FromJSON TodoItemSpec where
     title <- obj .: "title"
     description <- obj .:? "description"
     pure $ TodoItemSpec title description
+
+aesonOptions :: String -> Aeson.Options
+aesonOptions prefix =
+  Aeson.defaultOptions
+    { Aeson.fieldLabelModifier = constructorModifier prefix
+    }
+
+constructorModifier :: String -> String -> String
+constructorModifier prefix = lowerCaseFirst . drop lengthPrefix
+  where
+    lengthPrefix = length prefix
+    lowerCaseFirst (x : xs) = toLower x : xs
+    lowerCaseFirst "" = ""
